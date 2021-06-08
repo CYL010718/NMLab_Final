3704b3868d24bcc4926d26f8bb57bdf69e19f349
CYL <B06901062@ntu.edu.tw>
2021-06-08 18:15:54 +0800

    upd frontend

--- client/src/components/ModalComponents/Laboratory.js
+++ client/src/components/ModalComponents/Laboratory.js
@@ -1,131 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import BarrackPage from './BarrackPage'
+import LabPage from './LabPage'
 import { Button, Modal, Grid, Icon, Segment, Header, Input, Progress, Menu, Image } from 'semantic-ui-react';
 import soldierpng from '../../images/soldier_noback.png';
 
-const Barrack = ({ idx, x, y, cellState, buildingContract, labContract, account, updateCellState }) => {
-  const [ page, setPage ] = useState(0);
-  const [ level, setLevel ] = useState(1);
-  const [ soldierAmount, setSoldierAmount ] = useState(0);
-  const [ cannonAmount, setCannonAmount ] = useState(0);
-  const [ protectorAmount, setProtectorAmount ] = useState(0);
-  const [ spyAmount, setSpyAmount ] = useState(0);
-  const [ upgrading, setUpgrading ] = useState(false);
-  const [ upgradeProgress, setUpgradeProgress ] = useState(0);
-  const [ producing, setProducing ] = useState(false);
+const Laboratory = ({ idx, x, y, cellState, buildingContract, labContract, account, updateCellState }) => {
   
 
-  const getLevel = async () => {
-    const building = await buildingContract.methods.getBuildingById(cellState.index).call({from: account});
-    const exist = building[0];
-    const lv = building[2];
-    if(exist) {
-      console.log(`level: ${lv}`);
-      setLevel(lv);
-    }
-  }
-
-  const getAmounts = async () => {
-    const soldierNum = await barrackContract.methods.getSoldierAmount(account).call({from: account});
-    const cannonNum = await barrackContract.methods.getCannonAmount(account).call({from: account});
-    const protectorNum = await barrackContract.methods.getProtectorAmount(account).call({from: account});
-    const spyNum = await barrackContract.methods.getSpyAmount(account).call({from: account});
-    setSoldierAmount(soldierNum);
-    setCannonAmount(cannonNum);
-    setProtectorAmount(protectorNum);
-    setSpyAmount(spyNum);
-  }
-
-  const startUpgrade = async () => {
-    if(!buildingContract || !account) return;
-    const upgradingId = parseInt( await buildingContract.methods.getUpgradingId(account).call({from: account}) );
-    if(upgradingId !== 0 && upgradingId !== cellState.index) {
-      alert("something else are upgrading now!");
-      return;
-    }
-    await buildingContract.methods.startBuild(account, x, y).send({from: account});
-    const remainTime = parseInt( await buildingContract.methods.getRemainingTime(account).call({from: account}) );
-    console.log("upgrade remainTime: ", remainTime);
-    const newState = { ...cellState, upgrade: [0, remainTime] };
-    updateCellState(idx, newState);
-  }
-
-  const confirmUpgrade = async () => {
-    await buildingContract.methods.updateBuild(account).send({from: account});
-    const newState = { ...cellState, upgrade: false };
-    updateCellState(idx, newState);
-  }
-
   useEffect(() => {
-    if(buildingContract && barrackContract && account) {
-      getLevel();
-      getAmounts();
-    }
-    setProducing((cellState.soldierProduce || cellState.cannonProduce || cellState.protectorProduce || cellState.spyProduce) ?true:false);
-    //setProduceProgress(cellState.produce? cellState.produce[1]===0 ? 100 : cellState.produce[0]/cellState.produce[1]*100>100?100:cellState.produce[0]/cellState.produce[1]*100  : 0)
-    setUpgrading(cellState.upgrade?true:false);
-    setUpgradeProgress(cellState.upgrade? cellState.upgrade[1]===0 ? 100 : cellState.upgrade[0]/cellState.upgrade[1]*100>100?100:cellState.upgrade[0]/cellState.upgrade[1]*100  : 0);
-  }, [buildingContract, barrackContract, account, cellState])
+  
+    
+  }, [buildingContract, labContract, account, cellState])
 
   return <>
     <Modal.Content image>
       <Grid columns='equal' divided padded>
-        <Grid.Row centered>
-          <Menu pointing secondary>
-            <Menu.Item
-              name='Soldier'
-              active={page === 0}
-              onClick={()=>setPage(0)}
-            />
-            <Menu.Item
-              name='Cannon'
-              active={page === 1}
-              onClick={()=>setPage(1)}
-            />
-            <Menu.Item
-              name='Protector'
-              active={page === 2}
-              onClick={()=>setPage(2)}
-            />
-            <Menu.Item
-              name='Spy'
-              active={page === 3}
-              onClick={()=>setPage(3)}
-            />
-          </Menu>
-        </Grid.Row>
         <Grid.Row stretched>
-          <BarrackPage page = {page} idx = {idx} level = {level} upgrading = {upgrading} contract = {barrackContract} account = {account} cellState = {cellState} updateCellState = {updateCellState}/>
           <Grid.Column>
             <Header icon>
-              Barrack
+              Lab
               <br/>
               <Image size='massive' src={soldierpng} />
             </Header>
             <Segment padded color='black'>
-              <p>Produce Army  </p>
-              <p>Soldier amount: {soldierAmount}</p>
-              <p>Cannon amount: {cannonAmount}</p>
-              <p>Protector amount: {protectorAmount}</p>
-              <p>Spy amount: {spyAmount}</p>
+              <p>Upgrade Technology  </p>
             </Segment>
-            {
-              upgrading?
-              <>
-              <Header as='h4'>
-                Upgrade progress
-              </Header>
-              <Progress percent={upgradeProgress} indicating />
-              <div style={{textAlign: 'center'}}>
-                <Button primary disabled={upgradeProgress !== 100} onClick={() => confirmUpgrade()} >comfirm upgrade</Button>
-              </div>
-              </>
-              :
-              <div style={{textAlign: 'center'}}>
-                <Button primary disabled={producing} onClick={() => startUpgrade()} >start upgrade</Button>
-              </div>
-            }
           </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <LabPage idx = {idx} contract = {labContract} account = {account} cellState = {cellState} updateCellState = {updateCellState}/>
         </Grid.Row>
       </Grid>
     </Modal.Content>
@@ -133,4 +35,4 @@ const Barrack = ({ idx, x, y, cellState, buildingContract, labContract, account,
 
 }
 
-export default Barrack;
+export default Laboratory;
